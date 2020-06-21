fa31799fbe2fdad057aa293e960d640f86e1918f
benbenisme <benjamingeorgeking@gmail.com>
2020-06-21 15:29:22 +0100

    changed files

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,142 @@
-import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
+import React from 'react';
+import Toolbar from './toolbar/toolbar';
+import BackButton from './back-button/back-button';
+import BrowseSearchBar from './browse-search/browse-search-bar';
+import VolumePreview from './volume-preview/volume-preview';
+import {
+  Jumbotron,
+  Container,
+  Row
+} from 'reactstrap';
+import {
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import Volume from './volume/volume';
 import './App.css';
 
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
+ class App extends React.Component {
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      apiSearchResponse: '',
+      searchTerms: '',
+    };
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
+  handlePriceSearchAPICall = async (volumeInformation) => {
+    const identifiers = this.parseVolumeInformationForIdentifiers(volumeInformation);
+    const response = await fetch('/api/priceSearch', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ post: this.state.post }),
+      body: JSON.stringify({ post: identifiers}),
     });
     const body = await response.text();
+    console.log("here is the response: " + body);
+  }
 
-    this.setState({ responseToPost: body });
-  };
+  parseVolumeInformationForIdentifiers(volumeInformation) {
+    const industryIdentifiers = volumeInformation.volumeInfo.industryIdentifiers;
+    return {
+      isbn_13: industryIdentifiers[industryIdentifiers.map(e => e.type).indexOf('ISBN_13')].identifier,
+      isbn_10: industryIdentifiers[industryIdentifiers.map(e => e.type).indexOf('ISBN_10')].identifier,
+      title: volumeInformation.volumeInfo.title, 
+      publisher: volumeInformation.volumeInfo.publisher, 
+      publishedDate: volumeInformation.volumeInfo.publishedDate,
+    }
+  }
 
-  render() {
+  search = async (searchTerms) => {   
+    console.log(searchTerms.replace(/ /g, ''));
+    const apiKey = 'AIzaSyBi7vVICfe_DSTeOEis5nM-iKW0vDB2yC0';
+    const cleanAPISearchResponse = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${searchTerms.replace(/ /g, '')}&key=${apiKey}`
+    );
+    const apiSearchResponse = await cleanAPISearchResponse.json();
+    this.setState({apiSearchResponse: apiSearchResponse});
+    this.props.history.push('/volumePreviews');
+  }
+
+  render() {   
+    const response = this.state.apiSearchResponse.items ? this.state.apiSearchResponse.items : null;
+    const volumePreviewsDOM = response ? response.map((volume) => {
+      return (        
+          <Row>
+            <VolumePreview volumeResponse={volume} handlePriceSearchAPICall={this.handlePriceSearchAPICall}/>
+          </Row>
+      )
+    }) 
+    : <Jumbotron>
+    <h1 className="display-3">No Volumes Found</h1>
+    </Jumbotron>;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+      <div>
+        <Toolbar />
+
+        {/* Main page switch display */}
+        <Switch>
+
+          {/* Main Page Route Display*/}
+          <Route exact path="/">
+            <div>
+              <Jumbotron>
+                <h1 className="display-3">Kings Books</h1>
+                <p className="lead">Welcome to Kings Books, the online encyclopedia for books and reading material.</p>
+              </Jumbotron>
+
+              <p>{this.state.response}</p>
+              <form onSubmit={this.handleSubmit}>
+                <p>
+                  <strong>Post to Server:</strong>
+                </p>
+                <input
+                  type="text"
+                  value={this.state.post}
+                  onChange={e => this.setState({ post: e.target.value })}
+                />
+                <button type="submit">Submit</button>
+              </form>
+              <p>{this.state.responseToPost}</p>
+              
+            </div>
+          </Route>
+
+          {/* Browse Page Route Display */}
+          <Route path="/browse">
+            <div>
+              <BackButton />
+              <BrowseSearchBar onSearch={this.search} />
+            </div>
+          </Route>
+
+          {/* VolumePreviews Page Route Display */}
+          <Route path="/volumePreviews">
+            <div>
+              <BackButton />
+              <Container fluid>
+                {volumePreviewsDOM}
+              </Container>
+            </div>
+          </Route>
+
+          {/* Singluar Volume Page Route Display */}
+          <Route path="/volume">
+            <div>
+              <BackButton />
+              <Volume props={this.props.location.state} />
+            </div>
+          </Route>
+
+        </Switch>
       </div>
     );
   }
-}
+};
 
-export default App;
+export default withRouter(App);
