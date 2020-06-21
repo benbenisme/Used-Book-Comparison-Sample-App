7a4e5dd6d46d5c84593843342926e7319f155a99
benbenisme <benjamingeorgeking@gmail.com>
2020-06-21 15:31:55 +0100

    change server and index

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
@@ -9,4 +9,4 @@ ReactDOM.render(
       <Route component={App}/>
     </Router>,    
   document.getElementById('root')
-);
+);yar
